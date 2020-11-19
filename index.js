b75399f5ecec5e2e5a64de3846fdc6ca3f3512d9
Gido Skales <66243914+GidoSkales@users.noreply.github.com>
2020-11-19 15:49:13 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 // {}
 // []
+
+//Declaring Variables
 const charCount = document.getElementById('char');
 const textArea = document.getElementById('area');
 const max = 200;
 
 
+//The Onkeyup Event with function
 textArea.onkeyup = () => {
 	const counter = textArea.value.length;
 	console.log(counter);
 	charCount.innerHTML = counter;
-	
+//The if conditional to check for exceptions	
 	if(counter > max){
 		textArea.style.color = 'red';
 		textArea.value.slice(0,200);
 		console.log(textArea.value.slice(0,200));
 		return textArea.value.slice(0,200);
+		var warningId = document.getElementById('warn');
+		warningId.style.display = "block";
 	}
 	else {
+		var warningId = document.getElementById('warn');
 		textArea.style.color = '';
+		warningId.style.display = "";
 	}
 }
 
@@ -25,4 +32,5 @@ const toGGle = ()=> {
     const darK = document.body;
     darK.classList.toggle('dark');
 }
+//Calling the toggle function
 toGGle();
